448fcc0e620ebcf670751c5534d52b96199349c7
Valtteri <savonen.emppu@gmail.com>
2025-08-01 18:34:33 +0300

    fixing github integration

--- lib/github.ts
+++ lib/github.ts
@@ -5,6 +5,21 @@ const ORG = "builddrr-user-sites";
 const TEMPLATE_OWNER = "builddrr-user-sites";
 const TEMPLATE_REPO = "plain-nextjs-app";
 
+// Read more: https://github.com/orgs/community/discussions/26333
+async function waitForRepoReady(octokit: Octokit, org: string, repo: string, timeout = 5000) {
+  const start = Date.now();
+  while (Date.now() - start < timeout) {
+    try {
+      await octokit.request('GET /repos/{owner}/{repo}', { owner: org, repo });
+      return;
+    } catch (e) {
+      if (e instanceof Error && "status" in e && e.status !== 404) throw e;
+      await new Promise(res => setTimeout(res, 1000));
+    }
+  }
+  return;
+}
+
 // Set up Octokit with installation authentication
 async function getOctokitAsInstallation() {
   const auth = createAppAuth({
@@ -19,17 +34,17 @@ async function getOctokitAsInstallation() {
 }
 
 export async function createRepoFromTemplate(appName: string): Promise<string> {
+  console.log("Creating repo from template", TEMPLATE_REPO, appName);
   const octokit = await getOctokitAsInstallation();
-  const repoName = `${appName}`;
   const { data } = await octokit.request(
     `POST /repos/${TEMPLATE_OWNER}/${TEMPLATE_REPO}/generate`,
     {
       owner: ORG,
-      name: repoName,
+      name: appName,
       private: true,
-      // Octokit will handle headers and auth automatically
     }
   );
+  await waitForRepoReady(octokit, ORG, appName);
   return data.html_url;
 }
 
@@ -37,6 +52,7 @@ export async function uploadFilesToRepo(
   repo: string,
   files: Record<string, string> = {}
 ): Promise<void> {
+  console.log("Uploading files to repo", repo);
   const octokit = await getOctokitAsInstallation();
   for (const [path, content] of Object.entries(files)) {
     await octokit.request(`PUT /repos/${ORG}/${repo}/contents/${path}`, {
